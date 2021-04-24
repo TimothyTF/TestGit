ed64b1cd30538499cff88a17c36f325f59be7e8b
TimothyTF <timothytjien@hotmail.com>
2021-04-24 13:01:02 +0200

    Tweede taak is toegevoegd. En daarmee is de user story voltooid.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("taak 1 is voltooid.");
+        System.out.println("taak 2 is voltooid en btw daarmee de userstory.");
     }
 }
